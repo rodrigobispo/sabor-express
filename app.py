68a4681cf46a9905667aaef134be7b2e569e12d4
rodrigobispo <rodrigoaugustocb@gmail.com>
2024-07-02 14:40:59 -0300

    Estruturação em métodos.

--- app.py
+++ app.py
@@ -1,25 +1,36 @@
 import os
 
-print('''
+def exibir_nome_do_programa():
+    print('''
 𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤
 ''')
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
-
-opcao_escolhida = int(input('Escolha uma opção: '))
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
 def finalizar_app():
     os.system('clear')
     print('Encerrando o programa\n')
 
-if opcao_escolhida == 1:
-    print(f'Você escolheu a opção {opcao_escolhida} Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print(f'Você escolheu a opção {opcao_escolhida} Listar restaurantes')
-elif opcao_escolhida == 3:
-    print(f'Você escolheu a opção {opcao_escolhida} Ativar restaurantes')
-else:
-    finalizar_app()
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida == 1:
+        print(f'Você escolheu a opção {opcao_escolhida} Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print(f'Você escolheu a opção {opcao_escolhida} Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print(f'Você escolheu a opção {opcao_escolhida} Ativar restaurantes')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
