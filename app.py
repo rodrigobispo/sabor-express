bf9dfca7a850edc8d7472d3555c4c46a790f5b68
rodrigobispo <rodrigoaugustocb@gmail.com>
2024-07-02 15:03:33 -0300

    Exceção.

--- app.py
+++ app.py
@@ -15,19 +15,30 @@ def finalizar_app():
     os.system('clear')
     print('Encerrando o programa\n')
 
-def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal.')
+    main()
 
-    if opcao_escolhida == 1:
-        print(f'Você escolheu a opção {opcao_escolhida} Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print(f'Você escolheu a opção {opcao_escolhida} Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print(f'Você escolheu a opção {opcao_escolhida} Ativar restaurantes')
-    else:
-        finalizar_app()
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+
+        if opcao_escolhida == 1:
+            print(f'Você escolheu a opção {opcao_escolhida} Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print(f'Você escolheu a opção {opcao_escolhida} Listar restaurantes')
+        elif opcao_escolhida == 3:
+            print(f'Você escolheu a opção {opcao_escolhida} Ativar restaurantes')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('clear')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
