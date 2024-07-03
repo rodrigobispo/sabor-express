fe808bdcf26ce262931b0b475d487409ae1591b3
rodrigobispo <rodrigoaugustocb@gmail.com>
2024-07-03 12:11:30 -0300

    Prática de OO.

---
+++ app_restaurante.py
@@ -0,0 +1,32 @@
+from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+
+
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+bebida = Bebida('Suco de Melancia', 8.0, 'Grande')
+prato = Prato('Pão', 1.00, 'Pão francês')
+
+# restaurante_praca.adicionar_bebida_no_cardapio(bebida)
+# restaurante_praca.adicionar_prato_no_cardapio(prato)
+
+restaurante_praca.adicionar_no_cardapio(bebida)
+restaurante_praca.adicionar_no_cardapio(prato)
+
+# restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
+# restaurante_japones = Restaurante('Japa', 'Japonesa')
+
+# restaurante_mexicano.alternar_estado()
+
+# restaurante_praca.receber_avaliacao('Gui', 10)
+# restaurante_praca.receber_avaliacao('Lais', 4)
+# restaurante_praca.receber_avaliacao('Emy', 3)
+
+def main():
+    print(bebida)
+    print(prato, '\n')
+    restaurante_praca.exibir_cardapio
+    # Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
